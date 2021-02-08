2df149e8c84fc0204bfc584911cfc74ca9fd63c0
irostub <irostub@gmail.com>
2021-02-08 13:34:09 +0900

    Add router configuration for change

--- employee-frontend/src/App.js
+++ employee-frontend/src/App.js
@@ -1,11 +1,27 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="container">
-      <ListEmployeeComponent />
+    <div>
+      <Router>
+        <HeaderComponent />
+        <div className="container">
+          <Switch>
+            <Route path="/" component={ListEmployeeComponent}>
+              <ListEmployeeComponent />
+            </Route>
+            <Route path="/employees" component={ListEmployeeComponent}>
+              <ListEmployeeComponent />
+            </Route>
+          </Switch>
+        </div>
+        <FooterComponent />
+      </Router>
     </div>
   );
 }

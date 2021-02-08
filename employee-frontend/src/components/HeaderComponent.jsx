779c3085de0a368790d0e785b58a8f9293a03026
irostub <irostub@gmail.com>
2021-02-08 13:33:09 +0900

    Add header component

---
+++ employee-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class HeaderComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <header>
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+          <div>
+            <a href="#" className="navbar-brand">
+              LOGO
+            </a>
+          </div>
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default HeaderComponent;

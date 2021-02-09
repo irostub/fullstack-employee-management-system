c58e9825e88d007348cd9f12552a6729195efdb9
irostub <irostub@gmail.com>
2021-02-09 23:00:59 +0900

    Add route view employee component

--- employee-frontend/src/App.js
+++ employee-frontend/src/App.js
@@ -4,6 +4,7 @@ import ListEmployeeComponent from "./components/ListEmployeeComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
 import CreateEmployeeComponent from "./components/CreateEmployeeComponent";
+import ViewEmployeeComponent from "./components/ViewEmployeeComponent";
 import UpdateEmployeeComponent from "./components/UpdateEmployeeComponent";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -17,6 +18,7 @@ function App() {
             <Route path="/" exact component={ListEmployeeComponent}></Route>
             <Route path="/employees" component={ListEmployeeComponent}></Route>
             <Route path="/create-employee/:id" component={CreateEmployeeComponent}></Route>
+            <Route path="/view-employee/:id" component={ViewEmployeeComponent}></Route>
 
             {/* Do not use UpdateEmployeeComponent, too many overlap with CreateEmployeeComponet */}
             {/* <Route path="/update-employee/:id" component={UpdateEmployeeComponent}></Route> */}

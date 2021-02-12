dc4dd89c8903f77a1ec09b8d018dfc596f5e071f
irostub <irostub@gmail.com>
2021-02-12 15:02:59 +0900

    Fix cors origins localhost to heroku domain

--- employee-backend/src/main/java/com/example/employeebackend/controller/EmployController.java
+++ employee-backend/src/main/java/com/example/employeebackend/controller/EmployController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 //브라우저가 잡아내는 동일 출처 정책(Same-orgin policy)를 무시하기 위해 CrossOrigin 어노테이션으로 허용할 origins를 명시한다.
 //동일 출처 정책은 요청을 보낸 곳과 받는 곳이 서로 일치해야한다는 정책이며 일치한다는건 ip와 port 까지 다 같은 것을 의미한다.
 //하지만 이를 다 지키면 웹이 성립될리 없다.
-@CrossOrigin(origins="http://localhost:3000")
+@CrossOrigin(origins="https://ems-side-client.herokuapp.com",allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployController {

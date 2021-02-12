f1f9a45692f485a495a8d2cf729bf7ab208e34e7
irostub <irostub@gmail.com>
2021-02-12 15:04:21 +0900

    Add separate db uri property source

--- employee-backend/src/main/java/com/example/employeebackend/EmployeeBackendApplication.java
+++ employee-backend/src/main/java/com/example/employeebackend/EmployeeBackendApplication.java
@@ -2,8 +2,10 @@ package com.example.employeebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = { "classpath:database.properties" })
 public class EmployeeBackendApplication {
 
     public static void main(String[] args) {
